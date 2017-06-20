cb60e5afdec26e696299e2a0d0c84efdce91f9ac
Hays Clark <hays.clark@gmail.com>
2017-06-19 21:41:40 -0700

    upgrading from jade to pug

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(bodyParser.urlencoded({
 	extended: true
